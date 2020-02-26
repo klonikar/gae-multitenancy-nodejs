4b7c22f32d49b84c29d5d9ea57113a0c994a47b8
Kiran Lonikar <klonikar@yahoo-inc.com>
2020-02-26 22:35:03 +0530

    face registration UI

---
+++ public/face_rec/js/commons.js
@@ -0,0 +1,118 @@
+function requestExternalImage(url, updateElemId, postProcFunction) {
+  var img = new Image()
+  img.crossOrigin = "anonymous";
+  img.setAttribute('crossOrigin', 'anonymous');
+  img.onload = function() {
+    var canvas = document.createElement("canvas");
+    canvas.width = this.width;
+    canvas.height = this.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(this, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+
+    $(updateElemId).get(0).src = dataURL
+    postProcFunction()
+  }
+  img.src = url || $('#imgUrlInput').val();
+}
+
+function dataURItoBlob(dataURI) {
+    if(typeof dataURI !== 'string'){
+        throw new Error('Invalid argument: dataURI must be a string');
+    }
+    dataURI = dataURI.split(',');
+    var type = dataURI[0].split(':')[1].split(';')[0],
+        byteString = atob(dataURI[1]),
+        byteStringLength = byteString.length,
+        arrayBuffer = new ArrayBuffer(byteStringLength),
+        intArray = new Uint8Array(arrayBuffer);
+    for (var i = 0; i < byteStringLength; i++) {
+        intArray[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([intArray], {
+        type: type
+    });
+}
+
+function renderNavBar(navbarId, exampleUri) {
+  const examples = [
+    {
+      uri: 'login.html',
+      name: 'Login'
+    },
+    {
+      uri: 'faceExtraction.html',
+      name: 'Extract and Register Faces'
+    },
+    {
+      uri: 'faceRecognition.html',
+      name: 'Face Recognition Images'
+    },
+    {
+      uri: 'faceRecognitionWebcam.html',
+      name: 'Face Recognition Webcam'
+    },
+    {
+      uri: 'faceRecognitionVideo.html',
+      name: 'Face Recognition Video'
+    }
+  ]
+
+  const navbar = $(navbarId).get(0)
+  const pageContainer = $('.page-container').get(0)
+
+  const header = document.createElement('h3')
+  header.innerHTML = examples.find(ex => ex.uri === exampleUri).name
+  pageContainer.insertBefore(header, pageContainer.children[0])
+
+  const menuContent = document.createElement('ul')
+  menuContent.id = 'slide-out'
+  menuContent.classList.add('side-nav', 'fixed')
+  navbar.appendChild(menuContent)
+
+  const menuButton = document.createElement('a')
+  menuButton.href='#'
+  menuButton.classList.add('button-collapse', 'show-on-large')
+  menuButton.setAttribute('data-activates', 'slide-out')
+  const menuButtonIcon = document.createElement('img')
+  menuButtonIcon.src = 'images/menu_icon.png'
+  menuButton.appendChild(menuButtonIcon)
+  navbar.appendChild(menuButton)
+
+  examples
+    .forEach(ex => {
+      const li = document.createElement('li')
+      if (ex.uri === exampleUri) {
+        li.style.background='#b0b0b0'
+      }
+      const a = document.createElement('a')
+      a.classList.add('waves-effect', 'waves-light', 'pad-sides-sm')
+      a.href = ex.uri
+      const span = document.createElement('span')
+      span.innerHTML = ex.name
+      span.style.whiteSpace = 'nowrap'
+      a.appendChild(span)
+      li.appendChild(a)
+      menuContent.appendChild(li)
+    })
+
+  $('.button-collapse').sideNav({
+    menuWidth: 260
+  })
+}
+
+function renderSelectList(selectListId, onChange, initialValue, renderChildren) {
+  const select = document.createElement('select')
+  $(selectListId).get(0).appendChild(select)
+  renderChildren(select)
+  $(select).val(initialValue)
+  $(select).on('change', (e) => onChange(e.target.value))
+  $(select).material_select()
+}
+
+function renderOption(parent, text, value) {
+  const option = document.createElement('option')
+  option.innerHTML = text
+  option.value = value
+  parent.appendChild(option)
+}
---
+++ public/face_rec/js/faceMatcherBuilder.js
@@ -0,0 +1,22 @@
+// fetch features of faces from DB and build face matcher
+async function createFaceMatcher(numImagesForTraining = 1) {
+  const maxAvailableImagesPerClass = 5
+  numImagesForTraining = Math.min(numImagesForTraining, maxAvailableImagesPerClass)
+
+  const labeledFaceDescriptors = await Promise.all(classes.map(
+    async className => {
+      const descriptors = []
+      for (let i = 1; i < (numImagesForTraining + 1); i++) {
+        const img = await faceapi.fetchImage(getFaceImageUri(className, i))
+        descriptors.push(await faceapi.computeFaceDescriptor(img))
+      }
+
+      return new faceapi.LabeledFaceDescriptors(
+        className,
+        descriptors
+      )
+    }
+  ))
+
+  return new faceapi.FaceMatcher(labeledFaceDescriptors)
+}
