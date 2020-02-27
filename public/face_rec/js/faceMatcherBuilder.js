585ebb5983247a951f0befbe0cea947b7d6de05e
Kiran Lonikar <klonikar@yahoo-inc.com>
2020-02-27 14:10:45 +0530

    completed face recognition, added dummy FR webcam and video pages

--- public/face_rec/js/faceMatcherBuilder.js
+++ public/face_rec/js/faceMatcherBuilder.js
@@ -1,22 +1,31 @@
 // fetch features of faces from DB and build face matcher
-async function createFaceMatcher(numImagesForTraining = 1) {
-  const maxAvailableImagesPerClass = 5
-  numImagesForTraining = Math.min(numImagesForTraining, maxAvailableImagesPerClass)
-
-  const labeledFaceDescriptors = await Promise.all(classes.map(
-    async className => {
-      const descriptors = []
-      for (let i = 1; i < (numImagesForTraining + 1); i++) {
-        const img = await faceapi.fetchImage(getFaceImageUri(className, i))
-        descriptors.push(await faceapi.computeFaceDescriptor(img))
+async function createFaceMatcher(responseCallback) {
+    invokeGetAPI("/api/v1/faceFeatures/", true, function() {
+      
+      if(this.readyState == 4 && this.status == 200) {
+        let respObj = JSON.parse(this.responseText)
+        let faceFeatureMap = new Map()
+        respObj.response.forEach(entry => {
+            if(faceFeatureMap.has(entry.name)) {
+                //console.log(entry.name + " exists.")
+                faceFeatureMap.get(entry.name).push(new Float32Array(entry.features))
+            }
+            else {
+                //console.log(entry.name + " does not exist.")
+                faceFeatureMap.set(entry.name, [])
+                faceFeatureMap.get(entry.name).push(new Float32Array(entry.features))
+            }
+        })
+        const labeledFaceDescriptors = []
+        faceFeatureMap.forEach((v, k) => { labeledFaceDescriptors.push(new faceapi.LabeledFaceDescriptors(k, v)) })
+        window.faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors)
+        responseCallback()
       }
-
-      return new faceapi.LabeledFaceDescriptors(
-        className,
-        descriptors
-      )
-    }
-  ))
-
-  return new faceapi.FaceMatcher(labeledFaceDescriptors)
+      else if(this.readyState == 4){
+          let respObj = JSON.parse(this.responseText)
+          document.getElementById("updateStatus").innerHTML = "Failed to retrieve registered faces: " + respObj.message + ", error: " + respObj.error;
+          //alert("Error in invoking API. please retry.");
+      }
+  });
+  
 }
